1612a6a2f0fc63de4ed731d00265d67d9e825876
susanlovaglio <susan.lovaglio@gmail.com>
2016-04-16 19:30:21 -0400

    updated project files

---
+++ space/Constants.m
@@ -0,0 +1,15 @@
+//
+//  Constants.m
+//  Inspired By
+//
+//  Created by Susan on 4/1/16.
+//  Copyright © 2016 Susan Lovaglio. All rights reserved.
+//
+
+#import "Constants.h"
+
+@implementation Constants
+
+NSString *const NASA_KEY = @"OYwzjll9C9OcjZe4nT9JCq9Y5nl3HFVh9Rd5XOS6";
+
+@end
